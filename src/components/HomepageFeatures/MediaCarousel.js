6031fd307f634ccbf05ac72562014c3dfd7e5711
blackstar1453 <69788237+BlackStar1453@users.noreply.github.com>
2023-10-28 13:12:29 +0000

    Update MediaCarousel.js and styles.module.css

--- src/components/HomepageFeatures/MediaCarousel.js
+++ src/components/HomepageFeatures/MediaCarousel.js
@@ -11,51 +11,17 @@ const FeatureList = [
     ImgSrc: require("@site/static/img/image.png").default,
     description: (
       <>
-        GPT-Tutor可以实现几乎所有的单词学习方法，并且支持多语言单词记忆，且能够在电脑和手机上进行同步学习和记忆。一个浏览器扩展+Anki就相当于彩云小译+欧陆词典+多邻国+百词斩+Moji辞书···除此之外还有很多其它功能。
-      </>
-    ),
-  },
-  {
-    title: "单词解释并记忆",
-    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
-    description: (
-      <>
-        鼠标选取不认识的单词进行解释，然后一键将单词添加到anki，在电脑上或者同步到手机上记忆。
+        GPT-Tutor+Anki就相当于彩云小译+欧陆词典+多邻国+百词斩+Moji辞书···除此之外还有很多其它功能。
       </>
     ),
   },
   {
     title: "解释两个类似表达之间的细微差别",
-    ImgSrc: require("@site/static/img/gpt_tutor_1.png").default,
+    ImgSrc: require("@site/static/img/gpt_tutor_2.png").default,
     description: (
       <>
         轻松实现对单词的自定义解释。比如你可以要求解释单词的多个含义，近义词/反义词解释，语法搭配、词汇搭配以及相关的成语、谚语、俚语等等,甚至可以让GPT-Tutor在解释单词之前给你讲一个苏联笑话放松心情。
       </>
-    ),
-  },
-  {
-    title: "名词同类词扩充",
-    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
-    description: (
-      <>
-        你看见的任何所谓高效记单词方法，我们都可以通过创建一个prompt直接植入进GPT-Tutor中。视频中演示了GPT-Tutor植入的三种高效扩充单词词汇量的方法。
-      </>
-    ),
-  },
-  {
-    title: "形容词扩充",
-    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
-    description: (
-      <>
-        GPT-Tutor不只是能够帮助你学习单词，还能够帮助你学习语法、英语的地道表达和修改作文。
-      </>
-    ),
-  },
-  {
-    title: "多语言",
-    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
-    description: (
-      <>GPT-Tutor对于英语的学习最有帮助，但同样能够运用于学习其它语言。</>
     ),
   },
 ];
