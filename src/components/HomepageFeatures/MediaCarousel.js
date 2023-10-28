492e895087561aaa9b13d7ea02b9615d464b9018
blackstar1453 <69788237+BlackStar1453@users.noreply.github.com>
2023-10-28 06:36:13 +0000

    Update 7 files

--- src/components/HomepageFeatures/MediaCarousel.js
+++ src/components/HomepageFeatures/MediaCarousel.js
@@ -9,11 +9,7 @@ function CustomNextArrow(props) {
   const { onClick, style } = props;
   return (
     <div className={styles.arrowContainer}>
-      <div
-        className={styles.arrowStyle}
-        onClick={onClick}
-        style={{ ...style }}
-      >
+      <div className={styles.arrowStyle} onClick={onClick} style={{ ...style }}>
         &gt;
       </div>
     </div>
@@ -24,17 +20,12 @@ function CustomPrevArrow(props) {
   const { onClick, style } = props;
   return (
     <div className={styles.arrowContainer}>
-      <div
-        className={styles.arrowStyle}
-        onClick={onClick}
-        style={{ ...style }}
-      >
+      <div className={styles.arrowStyle} onClick={onClick} style={{ ...style }}>
         &lt;
       </div>
     </div>
   );
 }
-
 
 const FeatureList = [
   {
@@ -42,7 +33,7 @@ const FeatureList = [
     ImgSrc: require("@site/static/img/image.png").default,
     description: (
       <>
-        GPT-Tutor可以实现几乎所有的单词学习方法，并且支持多语言单词记忆，且能够在电脑和手机上进行同步学习和记忆。一个浏览器扩展+Anki就相当于彩云小译+欧陆词典+多邻国+百词斩+Moji辞书···除此之外还有很多其它功能。  
+        GPT-Tutor可以实现几乎所有的单词学习方法，并且支持多语言单词记忆，且能够在电脑和手机上进行同步学习和记忆。一个浏览器扩展+Anki就相当于彩云小译+欧陆词典+多邻国+百词斩+Moji辞书···除此之外还有很多其它功能。
       </>
     ),
   },
@@ -57,7 +48,7 @@ const FeatureList = [
   },
   {
     title: "解释两个类似表达之间的细微差别",
-    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+    Image: require("@site/static/img/gpt_tutor_1.png").default,
     description: (
       <>
         轻松实现对单词的自定义解释。比如你可以要求解释单词的多个含义，近义词/反义词解释，语法搭配、词汇搭配以及相关的成语、谚语、俚语等等,甚至可以让GPT-Tutor在解释单词之前给你讲一个苏联笑话放松心情。
@@ -96,13 +87,13 @@ const FeatureList = [
 function Feature({ Svg, title, description }) {
   return (
     <div className="features">
-        <div >
-          <Svg className={styles.featureSvg} role="img" />
-        </div>
-        <div className="featureText">
-         <h3>{title}</h3>
-         <p>{description}</p>
-        </div>
+      <div>
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="featureText">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
     </div>
   );
 }
@@ -110,13 +101,13 @@ function Feature({ Svg, title, description }) {
 function ImageFeature({ ImgSrc, title, description }) {
   return (
     <div className="features">
-        <div >
-          <img src={ImgSrc} className={styles.featureImg} role="img" />
-        </div>
-        <div className="featureText">
-          <h3>{title}</h3>
-          <p>{description}</p>
-        </div>
+      <div>
+        <img src={ImgSrc} className={styles.featureImg} role="img" />
+      </div>
+      <div className="featureText">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
     </div>
   );
 }
@@ -124,18 +115,16 @@ function ImageFeature({ ImgSrc, title, description }) {
 function GifFeature({ GifSrc, title, description }) {
   return (
     <div className="features">
-        <div>
-          <img src={GifSrc} className={styles.featureGif} alt={title} />
-        </div>
-        <div className="featureText">
-          <h3>{title}</h3>
-          <p>{description}</p>
-        </div>
+      <div>
+        <img src={GifSrc} className={styles.featureGif} alt={title} />
+      </div>
+      <div className="featureText">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
     </div>
   );
 }
-
-
 
 function MediaCarousel() {
   const settings = {
@@ -151,12 +140,12 @@ function MediaCarousel() {
   return (
     <div style={{ textAlign: "center" }}>
       <Slider {...settings}>
+        <h3>功能演示</h3>
         {FeatureList.map((feature, index) => (
           <div key={index}>
             {feature.Svg && <Feature {...feature} />}
             {feature.ImgSrc && <ImageFeature {...feature} />}
             {feature.GifSrc && <GifFeature {...feature} />}
-
           </div>
         ))}
       </Slider>
