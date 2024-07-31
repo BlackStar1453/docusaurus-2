c3a1a7e4fe1556cf6c3cc3a187a23bf72af6458b
BlackStar1453 <69788237+BlackStar1453@users.noreply.github.com>
2024-07-31 19:18:41 +0800

    修正字体的设置

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'h1': '3xl',  // 示例，根据需要自定义
+        'h2': '2xl',
+        'h3': 'xl',
+        'h4': 'lg',
+        'h5': 'md',
+        'h6': 'sm',
+      },
+      fontWeight: {
+        'h1': 'bold',
+        'h2': 'bold',
+      }
+    },
   },
   plugins: [],
 }
